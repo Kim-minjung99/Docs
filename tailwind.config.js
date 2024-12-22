067ae80323f842ad047700177d75c2f80e5965e1
minjung5229 <minjung5229@naver.com>
2024-12-22 22:41:18 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// [tailwind.config.cjs]
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./popup.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
